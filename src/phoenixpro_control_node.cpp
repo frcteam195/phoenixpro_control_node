6d443a1379e53722450359cab873955be934de2f
Robert Hilton <robert.a.hilton.jr@gmail.com>
2023-04-29 04:57:25 +0000

    Add base workings of phoenix pro and node templating

--- src/phoenixpro_control_node.cpp
+++ src/phoenixpro_control_node.cpp
@@ -1,10 +1,36 @@
+#include "phoenixpro_control_node/phoenixpro_control_node.hpp"
+#include "rclcpp/rclcpp.hpp"
+#define UNIT_LIB_DISABLE_FMT
+#define UNIT_LIB_ENABLE_IOSTREAM
+#include "ctre/phoenixpro/TalonFX.hpp"
 #include <cstdio>
+#include <string>
+#include <iostream>
 
-int main(int argc, char ** argv)
+#define NODE_NAME "phoenixpro_control_node"
+static constexpr char CAN_NAME[] = "jetsoncanivore1";
+
+
+using namespace ctre::phoenixpro;
+
+
+class LocalNode : public rclcpp::Node
 {
-  (void) argc;
-  (void) argv;
+public:
+    LocalNode() : rclcpp::Node(NODE_NAME)
+    {
 
-  printf("hello world phoenixpro_control_node package\n");
-  return 0;
+    }
+private:
+    hardware::TalonFX leftMaster{1, CAN_NAME};
+    hardware::TalonFX rightMaster{2, CAN_NAME};
+};
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<LocalNode>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
 }
