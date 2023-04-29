ad5104fdaddb319c52d8c95dbc2ec9888d16c3ff
Robert Hilton <robert.a.hilton.jr@gmail.com>
2023-04-29 05:35:41 +0000

    Work on cmake

--- src/phoenixpro_control_node.cpp
+++ src/phoenixpro_control_node.cpp
@@ -6,24 +6,34 @@
 #include <cstdio>
 #include <string>
 #include <iostream>
+#include "std_msgs/msg/string.hpp"
 
 #define NODE_NAME "phoenixpro_control_node"
 static constexpr char CAN_NAME[] = "jetsoncanivore1";
 
-
 using namespace ctre::phoenixpro;
-
+using std::placeholders::_1;
 
 class LocalNode : public rclcpp::Node
 {
 public:
     LocalNode() : rclcpp::Node(NODE_NAME)
     {
-
+        status_publisher = this->create_publisher<std_msgs::msg::String>("/MotorStatus", 10);
+        control_subscriber = this->create_subscription<std_msgs::msg::String>("/MotorControl", 1, std::bind(&LocalNode::control_msg_callback, this, _1));
     }
 private:
     hardware::TalonFX leftMaster{1, CAN_NAME};
     hardware::TalonFX rightMaster{2, CAN_NAME};
+
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr status_publisher;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr control_subscriber;
+
+    void control_msg_callback(const std_msgs::msg::String::SharedPtr msg)
+    {
+      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+    }
+
 };
 
 int main(int argc, char **argv)
