7cca6bac55cd2d2c865d5e247c5db27d4fc274d0
Robert Hilton <robert.a.hilton.jr@gmail.com>
2023-04-29 21:10:21 +0000

    Work on parameter loading

--- include/phoenixpro_control_node/phoenixpro_control_node.hpp
+++ include/phoenixpro_control_node/phoenixpro_control_node.hpp
@@ -1,6 +1,9 @@
 #pragma once
 #include "parameter_helper.hpp"
+#include "rclcpp/rclcpp.hpp"
 
+//TODO: In the future, auto generate this from the yaml file
 DEFINE_PARAMS(Parameters,
-    (canivore_name)
+    (canivore_name) ,
+    (rclcpp::PARAMETER_STRING)
 )
--- src/phoenixpro_control_node.cpp
+++ src/phoenixpro_control_node.cpp
@@ -1,18 +1,19 @@
 #include "phoenixpro_control_node/phoenixpro_control_node.hpp"
-#include "rclcpp/rclcpp.hpp"
-#define UNIT_LIB_DISABLE_FMT
-#define UNIT_LIB_ENABLE_IOSTREAM
-#include "ctre/phoenixpro/TalonFX.hpp"
 #include <cstdio>
 #include <string>
 #include <iostream>
 #include <vector>
+
+#include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "ck_ros2_base_msgs_node/msg/motor_status.hpp"
 #include "ck_ros2_base_msgs_node/msg/motor_status_array.hpp"
 
+#define UNIT_LIB_DISABLE_FMT
+#define UNIT_LIB_ENABLE_IOSTREAM
+#include "ctre/phoenixpro/TalonFX.hpp"
+
 #define NODE_NAME "phoenixpro_control_node"
-static constexpr char CAN_NAME[] = "jetsoncanivore1";
 
 using namespace ctre::phoenixpro;
 
@@ -21,24 +22,24 @@ class LocalNode : public rclcpp::Node
 public:
     LocalNode() : rclcpp::Node(NODE_NAME)
     {
-        params = this->get_parameters(param_names);
+        load_parameters(this);
+        CAN_NET = ParamMap[Parameters::canivore_name].as_string();
+        
+        leftMaster = new hardware::TalonFX(1, CAN_NET);
+        rightMaster = new hardware::TalonFX(2, CAN_NET);
 
         status_publisher = this->create_publisher<ck_ros2_base_msgs_node::msg::MotorStatusArray>("/MotorStatus", 10);
         control_subscriber = this->create_subscription<std_msgs::msg::String>("/MotorControl", 1, std::bind(&LocalNode::control_msg_callback, this, std::placeholders::_1));
     }
 private:
-    hardware::TalonFX leftMaster{1, CAN_NAME};
-    hardware::TalonFX rightMaster{2, CAN_NAME};
+    hardware::TalonFX* leftMaster;
+    hardware::TalonFX* rightMaster;
+
+    std::string CAN_NET = "";
 
     rclcpp::Publisher<ck_ros2_base_msgs_node::msg::MotorStatusArray>::SharedPtr status_publisher;
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr control_subscriber;
 
-    std::vector<std::string> param_names = {
-        "my_str",
-        "my_int",
-        "my_double_array"
-    };
-    std::vector<rclcpp::Parameter> params;
 
     void control_msg_callback(const std_msgs::msg::String::SharedPtr msg)
     {
